0715945dc3d3f252bf3cd927183fd8a09c09b87b
m0sk1t <m0sk1t@bk.ru>
2016-02-24 12:16:58 +0300

    server started

---
+++ app.js
@@ -0,0 +1,26 @@
+var app = require('express')(),
+	favicon = require('serve-favicon'),
+	bodyParser = require('body-parser'),
+	compression = require('compression'),
+	serveStatic = require('serve-static'),
+	cookieParser = require('cookie-parser');
+
+process.on('uncaughtException', function(err) {
+	console.log((new Date()) + err + '\r\n');
+	console.log((new Date()) + 'Error stack: ' + err.stack + '\r\n');
+});
+app.use(busboy());
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({
+	limit: '50mb',
+	extended: true
+}));
+app.use(bodyParser.json({
+	limit: '50mb'
+}));
+app.use(serveStatic(__dirname + '/pub'));
+//app.use(favicon(__dirname + "/pub/img/favicon.ico"));
+app.listen(8088, function() {
+	console.log("Started");
+})
---
+++ pub/script.js
@@ -0,0 +1,27 @@
+(function() {
+	var currentSection = 0;
+	$('#right').on('click', function() {
+		turnSection(1);
+	});
+	$('#left').on('click', function() {
+		turnSection(-1);
+	});
+
+	function turnSection(dir) {
+		if (dir === -1) {
+			if (currentSection === 0) {
+				currentSection = $('section').length - 1;
+			} else {
+				currentSection += dir;
+			}
+		} else {
+			if (currentSection === $('section').length - 1) {
+				currentSection = 0;
+			} else {
+				currentSection++;
+			}
+		}
+		$('#sec0').css('margin-left', '-' + currentSection + '00%');
+		$('article').css('background-position', (100 / $('section').length) * currentSection + '% 0');
+	}
+})();
